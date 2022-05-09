5c8bb6ec85a48b2b63244a8c7ffff971080a42a7
metruzanca <samuele@zanca.dev>
2022-05-09 09:50:43 -0400

    POC

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,15 +1,14 @@
 <script context="module" lang="ts">
   import type { LoadOutput } from "@sveltejs/kit";
+  const files = import.meta.glob("./**/*.md");
+
+  let body: any = [];
+
+  for (const path in files) {
+    body.push(files[path]().then(({ metadata }) => metadata));
+  }
 
   export async function load(): Promise<LoadOutput> {
-    const files = import.meta.glob("./**/*.md");
-
-    let body: any = [];
-
-    for (const path in files) {
-      body.push(files[path]().then(({ metadata }) => metadata));
-    }
-
     const posts = await Promise.all(body);
     return {
       props: {
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<h1>Zanca.dev</h1>
+
+Hey, theres no home page (theres also no styles currently)
+<p>
+  Please go <a class="text-blue-500 hover:underline" href="/blog">here</a> for content
+</p>
