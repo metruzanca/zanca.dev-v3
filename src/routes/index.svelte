92c40ff6aa3b052f747aba900e38f029c571b840
metruzanca <samuele@zanca.dev>
2022-05-15 02:21:02 -0400

    SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,8 @@
       props: { posts },
     };
   }
+  const title = "Developer Blog by Samuele Zanca";
+  const url = "Zanca.Dev";
 </script>
 
 <script lang="ts">
@@ -15,6 +17,21 @@
   import { formatDate } from "$lib/dates";
   export let posts: Post[];
 </script>
+
+<svelte:head>
+  <meta name="theme-color" content="#f7e13a" />
+  <meta name="description" content={title} />
+  <!-- Open Graph -->
+  <meta property="og:title" content="{url} - {title}" />
+  <meta property="og:description" content={title} />
+  <meta property="og:type" content="website" />
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:creator" content="metruzanca" />
+  <meta name="twitter:description" content={title} />
+  <meta name="twitter:title" content="{url} - {title}" />
+  <title>{url} | {title}</title>
+</svelte:head>
 
 <main class="mx-auto w-[600px]">
   <ul class="list-none">
