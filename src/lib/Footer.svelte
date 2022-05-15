60ae938e6c12c7d05a834b236cf8a4e8eb09d084
metruzanca <samuele@zanca.dev>
2022-05-15 01:24:38 -0400

    hidden the ugly as nails footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,8 @@
-<hr />
+<!-- This footer is beyond ugly so I'm actually gonna hide it for now -->
 
-<h5>Written by Samuele Zanca</h5>
+<!-- <hr />
+
+<h5>Written by Samuele Zanca</h5> -->
 
 <!-- TODO previous article -->
 <!-- TODO next article button -->
