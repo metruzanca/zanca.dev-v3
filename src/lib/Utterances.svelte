9228d5e4aa7f9e78c98512139b55382026935f98
metruzanca <samuele@zanca.dev>
2022-05-15 00:30:38 -0400

    Removed utterances. I wanna move to ghost cms or similar

--- src/lib/Utterances.svelte
+++
@@ -1,8 +0,0 @@
-<script src="https://utteranc.es/client.js"
-        repo="metruzanca/zanca.dev-svelte"
-        issue-term="url"
-        label="blog-comments"
-        theme="dark-blue"
-        crossorigin="anonymous"
-        async>
-</script>
--- src/routes/blog/__layout.svelte
+++ src/routes/blog/__layout.svelte
@@ -1,23 +1,10 @@
 <script>
-  // @ts-nocheck (utterances)
-  import { browser } from "$app/env";
   import Center from "$lib/Center.svelte";
   import Footer from "$lib/Footer.svelte";
-
-  import { Utterances } from "utterances-svelte-component";
 </script>
 
 <Center>
   <slot />
 </Center>
-
-{#if browser}
-  <Utterances
-    repo="metruzanca/zanca.dev-svelte"
-    issueTerm="url"
-    label="blog-comments"
-    theme="dark-blue"
-  />
-{/if}
 
 <Footer />
