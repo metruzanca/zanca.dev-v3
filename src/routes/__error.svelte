d7bfe4961c155f823fa1e9a8d2d0650292ca48a9
metruzanca <samuele@zanca.dev>
2022-05-17 16:58:43 -0400

    error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = async ({ error, status }) => {
+    console.error(error);
+    return {
+      props: { status, message: error?.message },
+    };
+  };
+</script>
+
+<script lang="ts">
+  import Glitch from "$lib/Glitch.svelte";
+
+  export let status: string;
+  export let message: string;
+</script>
+
+<main class="mx-auto max-w-[800px] flex flex-col items-center">
+  <h1>
+    <Glitch glitchText={status} />
+  </h1>
+  <p class="p-8 text-xl">{message}</p>
+  <!-- TODO improve link buttons -->
+  <a href="/" class="text-l border-2 border-current p-2 rounded-lg">Go Home</a>
+</main>
