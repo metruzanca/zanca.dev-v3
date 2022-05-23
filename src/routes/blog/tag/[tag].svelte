d7f32c64ba86785e9ce8273ea7dca738c41b6c64
metruzanca <samuele@zanca.dev>
2022-05-23 00:01:03 -0400

    Removed some comments

--- src/routes/blog/tag/[tag].svelte
+++ src/routes/blog/tag/[tag].svelte
@@ -1,18 +1,3 @@
-<!-- <script context="module" lang="ts">
-  import type { Load } from "@sveltejs/kit";
-  import type { Tag, TagList } from "src/app";
-  import tags from "./tags.json";
-
-  export const load: Load = async ({ params }) => {
-    const tag = (tags as unknown as TagList)[params.tag];
-    if (!tag) {
-      throw new Error(`Tag ${params.tag} not found.`);
-    }
-    return {
-      props: { tag },
-    };
-  };
-</script> -->
 <script lang="ts">
   import PostList from "$lib/components/PostList.svelte";
   import type { Post, Tag } from "src/app";
