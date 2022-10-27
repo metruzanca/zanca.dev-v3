a7276d307c9bcb17b2e9c7b994c846914c3ff759
metruzanca <sam@zanca.dev>
2022-10-26 20:09:20 -0400

    Added resume route

---
+++ src/routes/resume/index.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { MetaTags } from "svelte-meta-tags";
+  import { BASE_URL, OPEN_GRAPH, TITLE, TWITTER, URL } from "$lib/constants";
+  import { browser } from "$app/env";
+
+  if(browser) {
+    window.location.href = "https://gist.githubusercontent.com/metruzanca/751361e5ba58ad06f361ebd430ae6e10/raw/resume.pdf";
+  }
+</script>
+
+<MetaTags
+  titleTemplate="{URL} - %s"
+  title={TITLE}
+  description={TITLE}
+  canonical={BASE_URL}
+  twitter={{
+    ...TWITTER,
+    description: TITLE,
+    title: TITLE,
+  }}
+  openGraph={{
+    ...OPEN_GRAPH,
+    title: TITLE,
+    description: TITLE,
+    type: "website",
+  }}
+/>
